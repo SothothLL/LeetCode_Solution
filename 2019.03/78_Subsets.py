54649da8c79a0a6ec0fd400eada775c1fd9bd47c
SothothLL <396225010@qq.com>
2019-03-17 23:20:03 +0800

    Update

---
+++ 2019.03/78_Subsets.py
@@ -0,0 +1,7 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for i in range(len(nums)):
+            for j in range(len(res)):
+                res.append(res[j]+[nums[i]])
+        return res
