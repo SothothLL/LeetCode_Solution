94ac3d2d24a81d880a0b115270346ef732e6bfdc
SothothLL <396225010@qq.com>
2019-03-23 23:44:57 +0800

    Update

--- 2019.03/70_Climbing Stairs.py
+++ 2019.03/70_Climbing Stairs.py
@@ -1,10 +1,8 @@
 # 直接递归提示超时,设置缓存
+# 归纳发现是斐波那契数列 F(n)=F(n-1)+F(n-2)
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 2 or n == 1:
-            return n
-        else:
-            temp = [1, 2]
-            for i in range(2, n):
-                temp.append(temp[i-1] + temp[i-2])
-            return temp[-1]
+        a, b = 1, 1
+        for i in range(n):
+            a, b = b, a+b
+        return a
