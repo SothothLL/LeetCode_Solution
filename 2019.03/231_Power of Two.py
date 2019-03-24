501d691554b68afb36e54076888da23cf4fece19
SothothLL <396225010@qq.com>
2019-03-24 21:17:47 +0800

    Update

--- 2019.03/231_Power of Two.py
+++ 2019.03/231_Power of Two.py
@@ -3,8 +3,6 @@
 
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n == 0:
+        if n < 1:
             return False
-        elif n == 1:
-            return True
         return n & (n-1) == 0
